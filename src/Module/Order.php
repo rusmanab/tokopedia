61e0e9fea8d5f73667b4057f0472374f07711228
rusmanab <rusmanab@gmail.com>
2021-03-04 10:29:42 +0700

    add fitur order

--- src/Module/Order.php
+++ src/Module/Order.php
@@ -4,8 +4,10 @@ namespace Rusmanab\Tokopedia\Module;
 use Rusmanab\Tokopedia\ModuleAbstract;
 
 class Order extends ModuleAbstract{
-    public function getAll(){
-        $url = "/v2/order/list?fs_id=$this->fsId&shop_id=$this->shopId&from_date=1491623331&to_date=1554695331&page=1&per_page=1";
+    public function getAll($parameter = []){
+        $from_date = isset($parameter['from_date']) ? $parameter['from_date'] : strtotime(date(now())) ;
+        $to_date   = $parameter['to_date'] ? $parameter['to_date'] : strtotime(date(now())) ;
+        $url = "/v2/order/list?fs_id=$this->fsId&shop_id=$this->shopId&from_date=$from_date&$to_date=1554695331&page=1&per_page=1";
         return $this->post($url, [], "GET");
     }
 }
