8245fe62e27551277fe3944e95657671ece7392a
rusmanab <rusmanab@gmail.com>
2021-10-12 08:46:03 +0700

    fixing

--- src/Module/Logistic.php
+++ src/Module/Logistic.php
@@ -7,7 +7,7 @@ class Logistic extends ModuleAbstract{
 
     public function getCourier($parameter = []){
      
-        $url = "v1/logistic/fs/$this->fsId/active-info";
+        $url = "/v1/logistic/fs/$this->fsId/active-info";
         return $this->post($url,[], $parameter, "GET");
     }
 
